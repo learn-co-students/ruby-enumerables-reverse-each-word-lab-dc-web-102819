adcccec4f4bd9ee5f9fd007653ffd11336a074b1
Kyle Cole <kylepcole@gmail.com>
2019-10-19 18:46:35 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(string)
+  wordArray = string.split(" ")
+  
+  characterArray = wordArray.collect{|word| word.reverse!}
+  #characterArray = []
+  #wordArray.each{|word| characterArray.push(word.reverse!)}
+  puts characterArray.join(" ")
+  characterArray.join(" ")
+end
