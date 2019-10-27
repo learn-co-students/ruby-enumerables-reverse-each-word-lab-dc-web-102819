7a0c82c2c14d049f14d47a75fc38db02143e805f
Amia Defreitas <amia.defreitas0@gmail.com>
2019-10-27 17:02:10 +0000

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -77,4 +77,10 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 =end
+def reverse_each_word(sentence)
+  reversed = sentence.split(" ").collect do |word|
+  word.reverse
+end
+  reversed.join(" ")
+end 
 end
